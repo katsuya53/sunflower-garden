6e62bc703d67dc2f37820a511a97420a5f0c810d
katsuya53 <redstar53hk@gmail.com>
2024-03-11 00:29:11 +0900

    userテーブル作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   
-  get 'prototypes/index'
   root to: "posts#index"
   
 end
--- db/migrate/20240310151940_devise_create_users.rb
+++ db/migrate/20240310151940_devise_create_users.rb
@@ -4,8 +4,10 @@ class DeviseCreateUsers < ActiveRecord::Migration[7.0]
   def change
     create_table :users do |t|
       ## Database authenticatable
+      t.string :nickname,           null: false
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
+      t.integer :prefecture_id,     null: false
 
       ## Recoverable
       t.string   :reset_password_token
